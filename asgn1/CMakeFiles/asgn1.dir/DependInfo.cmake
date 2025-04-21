035d20367eb27b1c4f3f9cb148a2d077b0df4122
root <root@DESKTOP-9CEIGS3.>
2025-04-21 12:00:10 +0300

    collision types added

---
+++ asgn1/CMakeFiles/asgn1.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/root/advanced_cpp/advanced-cpp/asgn1/src/game/GameBoard.cpp" "CMakeFiles/asgn1.dir/src/game/GameBoard.cpp.o" "gcc" "CMakeFiles/asgn1.dir/src/game/GameBoard.cpp.o.d"
+  "/root/advanced_cpp/advanced-cpp/asgn1/src/game/GameManager.cpp" "CMakeFiles/asgn1.dir/src/game/GameManager.cpp.o" "gcc" "CMakeFiles/asgn1.dir/src/game/GameManager.cpp.o.d"
+  "/root/advanced_cpp/advanced-cpp/asgn1/src/game/Logger.cpp" "CMakeFiles/asgn1.dir/src/game/Logger.cpp.o" "gcc" "CMakeFiles/asgn1.dir/src/game/Logger.cpp.o.d"
+  "/root/advanced_cpp/advanced-cpp/asgn1/src/main.cpp" "CMakeFiles/asgn1.dir/src/main.cpp.o" "gcc" "CMakeFiles/asgn1.dir/src/main.cpp.o.d"
+  "/root/advanced_cpp/advanced-cpp/asgn1/src/objects/GameObject.cpp" "CMakeFiles/asgn1.dir/src/objects/GameObject.cpp.o" "gcc" "CMakeFiles/asgn1.dir/src/objects/GameObject.cpp.o.d"
+  "/root/advanced_cpp/advanced-cpp/asgn1/src/objects/MovingElement.cpp" "CMakeFiles/asgn1.dir/src/objects/MovingElement.cpp.o" "gcc" "CMakeFiles/asgn1.dir/src/objects/MovingElement.cpp.o.d"
+  "/root/advanced_cpp/advanced-cpp/asgn1/src/objects/Tank.cpp" "CMakeFiles/asgn1.dir/src/objects/Tank.cpp.o" "gcc" "CMakeFiles/asgn1.dir/src/objects/Tank.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
